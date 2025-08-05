288290111c5b1d0977f0715c5db03e696bb980f8
shankham <32229261+shankham@users.noreply.github.com>
2025-08-06 02:01:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Beautiful World!");
+        System.out.println("Hello, our beautiful World!");
     }
 }
